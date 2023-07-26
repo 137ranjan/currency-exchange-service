2169955e58761a2799c0ff12570c2c7466c179db
Ranjan Singh <137ranjan@gmail.com>
2023-07-26 14:25:02 +0530

    first commit

---
+++ src/test/java/com/ranjan/microservice/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjan.microservice.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

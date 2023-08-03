65ab9591195ea395318a3fe625d93fac0f6d922a
Ranjan Singh <137ranjan@gmail.com>
2023-08-03 17:12:00 +0530

    Connecting Currency Exchange Microservice with Zipkin

--- src/main/java/com/ranjan/microservice/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/ranjan/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.ranjan.microservice.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private CurrencyExchangeRepository repository;
     @Autowired
@@ -17,9 +20,10 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
-        if(currencyExchange == null){
-            throw new RuntimeException("Unable to find data for "+from+" to "+to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
         }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
